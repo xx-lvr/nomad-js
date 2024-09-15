91cc6a31a65959312d538c7ab2b3b421958884d8
jsw9436 <jswjsw101010@gmail.com>
2024-09-15 13:37:45 +0900

    Nomad

---
+++ quotes.js
@@ -0,0 +1,21 @@
+const quotes = [
+    {
+        quoto: "오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다",
+        author: "앙드레 말로",
+    },
+    {
+        quoto: "평생 살 것처럼 꿈을 꾸어라.그리고 내일 죽을 것처럼 오늘을 살아라",
+        authgor: "제임스 딘",
+    },
+    {
+        quoto: "1퍼센트의 가능성, 그것이 나의 길이다",
+        author: "나폴레옹",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.legnth)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
